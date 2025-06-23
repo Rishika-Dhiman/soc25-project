6a280d79b7bc7a44e76bd170e421e6cee316b798
Rishika-Dhiman <rishikadhiman1205@gmail.com>
2025-06-23 18:23:08 +0530

    Added control with arrow keys and deleted unnecessary variables

--- Assets/GroundScript.cs
+++ Assets/GroundScript.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 public class GroundScript : MonoBehaviour
 
 {
-    public Rigidbody2D myRigidbody;
     public GameObject ground;
-    public float startTime;
-    public float downSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/PlatformScript.cs
+++ Assets/PlatformScript.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
 {
-    public Rigidbody2D myRigidBody;
-    public float downSpeed=3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
