0615011d34a9b5862adaee8faea4366692ca8915
Rishika-Dhiman <rishikadhiman1205@gmail.com>
2025-07-01 17:24:07 +0530

    Spawning players

--- Assets/GroundScript.cs
+++ Assets/GroundScript.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class GroundScript : MonoBehaviour
+public class GroundScript : MonoBehaviourPunCallbacks
 
 {
     public GameObject ground;
+    public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Instantiate(ground, transform.position + Vector3.right*2,transform.rotation);
+
+        Vector3 spawnPos = new Vector3(Random.Range(-2f, 2f), 3f, 0f);
+        PhotonNetwork.Instantiate(player.name, spawnPos, Quaternion.identity);
     }
 
     // Update is called once per frame
