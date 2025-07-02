6007ffd0460863a381e56a0be0764c1cb062c1aa
Rishika-Dhiman <rishikadhiman1205@gmail.com>
2025-07-02 17:05:41 +0530

    Used Invoke() to delay assignment of player object

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -4,29 +4,25 @@ public class CameraScript : MonoBehaviour
 {
     public GameObject ground;
     Transform playerTransform;
+    bool set = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
-         playerTransform=GroundScript.localPlayer.transform;
+        Invoke("PlayerTransform",1);
+         
+    }
+
+    void PlayerTransform()
+    {
+        playerTransform = GroundScript.localPlayer.transform;
+        set = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (playerTransform == null)
-        {
-            // Wait until localPlayer is assigned
-            if (GroundScript.localPlayer != null)
-            {
-                playerTransform = GroundScript.localPlayer.transform;
-            }
-            return;
-        }
-
-        if (playerTransform.position.y > 0)
+        if (set && playerTransform.position.y > 0 )
         { 
             transform.position = new Vector3(transform.position.x, playerTransform.position.y, transform.position.z);    
         }
